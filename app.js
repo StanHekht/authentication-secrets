ebdc1d5f37e11fcee4f3521d8a3c18258262a4f9
Stanislau Hekht <stanislauhekht@Stanislaus-iMac.fios-router.home>
2020-10-09 01:59:03 -0400

    Temporarily empty out login and register post routes to prepare for cookie and session implementation

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+
 
 const app = express();
 
@@ -40,43 +39,11 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-    const newUser = new User ({
-      email: req.body.username,
-      password: hash
-    })
-    newUser.save((err) => {
-      if (err){
-        console.log(err);
-      } else {
-        res.render('secrets');
-      }
-    });
-  });
-
-
+  
 });
 
 app.post("/login", (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
 
-  User.findOne({
-    email: username
-  }, (err, foundUser) => {
-    if (err){
-      console.log(err);
-    } else {
-      if(foundUser){
-        bcrypt.compare(password, foundUser.password, (err, result) => {
-          if (result === true){
-            res.render("secrets");
-          }
-        });
-
-      }
-    }
-  });
 });
 
 
